f0cf7ed548a9effae284949726023ba6a1e6993f
Joey Smith <jsmith@webinertia.net>
2023-03-25 06:01:48 -0500

    Signed-off-by: Joey Smith <jsmith@webinertia.net>

--- test/View/Helper/AbstractCommonTestCase.php
+++ test/View/Helper/AbstractCommonTestCase.php
@@ -29,12 +29,14 @@ abstract class AbstractCommonTestCase extends TestCase
     {
         Doctype::unsetDoctypeRegistry();
         $this->sm            = new ServiceManager();
+
         $this->renderer      = new PhpRenderer();
         $helperPluginManager = $this->renderer->getHelperPluginManager();
         $viewHelperConfig    = new Config((new ConfigProvider())->getViewHelperConfig(), true);
         $bootstrapConfig     = new Config((new BootstrapConfig())->getViewHelperConfig(), true);
         $merged              = $viewHelperConfig->merge($bootstrapConfig);
         $helperPluginManager->configure($merged->toArray());
+
         $this->sm->setService('ViewHelperManager', $helperPluginManager);
         $this->renderer->setHelperPluginManager($helperPluginManager);
 
