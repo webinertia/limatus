22880d61e64c9f171319754815c2c2447c7408a6
Joey Smith <jsmith@webinertia.net>
2023-04-17 02:55:26 -0500

    Adding support for inline mode Signed-off-by: Joey Smith <jsmith@webinertia.net>
    Signed-off-by: Joey Smith <jsmith@webinertia.net>

--- src/Form/View/Helper/FormInput.php
+++ src/Form/View/Helper/FormInput.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Limatus\Form\View\Helper;
 
+use Laminas\Filter\Word\SeparatorToSeparator;
 use Laminas\Form\ElementInterface;
 use Laminas\Form\Exception;
 use Limatus\Form;
@@ -84,6 +85,10 @@ class FormInput extends AbstractHelper
 
     protected static string $horizontalWrapper = '<div %s>%s</div>';
 
+    protected static string $inputGroupWrapper = '<div class="input-group">%s%s</div>';
+
+    protected static string $inputGroupTextWrapper = '<div class="input-group-text">%s</div>';
+
     /**
      * Invoke helper as functor
      *
@@ -122,6 +127,20 @@ class FormInput extends AbstractHelper
                     $this->createAttributesString($element->getHorizontalAttributes()),
                     $markup
                 );
+            }
+            if (Form\ModeAwareInterface::INLINE_MODE === $mode) {
+                $options = $element->getOptions();
+                if (isset($options['input_group']) && isset($options['input_group_text'])) {
+                    $inputText = sprintf(
+                        self::$inputGroupTextWrapper,
+                        $options['input_group_text'],
+                    );
+                    $markup = sprintf(
+                        self::$inputGroupWrapper,
+                        $inputText,
+                        $markup
+                    );
+                }
             }
         }
 
