69e4491c39cda9459cc989f73820cbddae7d0664
Joey Smith <jsmith@webinertia.net>
2023-06-15 13:32:28 -0500

    Adding password element Adding webinertia-validator as a dep Updated laminas-form dep to laminas-mvc-form 2.1.1 Signed-off-by: Joey Smith <jsmith@webinertia.net>
    Signed-off-by: Joey Smith <jsmith@webinertia.net>

---
+++ src/Form/Element/Password.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Limatus\Form\Element;
+
+use Laminas\Form\ElementPrepareAwareInterface;
+use Laminas\Form\FormInterface;
+use Limatus\Form\Element;
+
+final class Password extends Element implements ElementPrepareAwareInterface
+{
+    protected $attributes = [
+        'type' => 'password',
+    ];
+
+    public function prepareElement(FormInterface $form): void
+    {
+        $this->setValue('');
+    }
+}
